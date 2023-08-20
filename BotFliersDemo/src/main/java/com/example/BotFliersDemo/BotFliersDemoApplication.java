764f55911d4694d4f18b1a833d5f3771d6548756
Sa1dzhan <119064156+Sa1dzhan@users.noreply.github.com>
2023-08-20 15:01:29 +0600

    initial commit

---
+++ BotFliersDemo/src/main/java/com/example/BotFliersDemo/BotFliersDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BotFliersDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFliersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotFliersDemoApplication.class, args);
+	}
+
+}
---
+++ BotFliersDemo/src/test/java/com/example/BotFliersDemo/BotFliersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BotFliersDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotFliersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
